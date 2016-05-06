650518ff94f3789a02e5bde53f49af4e3e93d569
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2016-05-06 13:42:58 -0500

    Craete view thanks

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.default')
+@section('content-wrapper')
+<div class="contain__def center">
+  <h1 class="title__pages font-blue center relaway">Felicitaciones.</h1>
+<section class="contain__information center">
+
+
+
+
+
+
+
+  <p class="font-gray relaway margin">
+    Estas sucrito a nuestro boletin semanal,
+    recibiras informacion actual de tu interes.
+  </p>
+
+<section class="contain__information center-thanks">
+    <img src="img/check.png" alt="" class="center-thanks"/>
+</section>
+<p class="font-gray  relaway center margin">
+  World Talent Consulting: nace como una empresa dedicada a ofrecer soluciones especializadas en Consultoría, Headhunting, Coaching y Outplacement. Hoy en día se he convertido en una empresa de consultoría global para el mercado Latinoamericano y Norteamericano.
+
+<br>
+</p>
+<br>
+  </section>
+  </div>
+@stop
